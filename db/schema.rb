18bd74741325e27f183212dfa98f946587ebac21
gorety <wagutugorety@gmail.com>
2023-08-01 18:36:23 +0300

    addavatar column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_131834) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_134818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_29_131834) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   add_foreign_key "itineraries", "destinations"
