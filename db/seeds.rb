44816ab8d146ef2d2a3905db86689d70a2591b18
alvinkenyagah <alvin47k@gmail.com>
2023-08-03 18:21:43 +0300

    Add admin controller methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# admin1 = Admin.create({first_name:"Alvin",last_name:"Kenyagah",username:"alvinkenyagah",email:"alvin47k@gmail.com",password:"alvinkenyagah", avatar:"https://zeldor.biz/wp-content/uploads/2012/10/ruby_logo.png"})
+# admin2 = Admin.create({first_name:,last_name:,username:,email:,password:})
+# admin3 = Admin.create({first_name:,last_name:,username:,email:,password:})
+# admin4 = Admin.create({first_name:,last_name:,username:,email:,password:})
+# admin5 = Admin.create({first_name:,last_name:,username:,email:,password:})
+
+
